4cadf91394dd9c52886dbe3c6f106332c373648f
rjjrbatarao <50077340+rjjrbatarao@users.noreply.github.com>
2026-07-22 11:08:14 +0800

    Add files via upload

--- menu/index.js
+++ menu/index.js
@@ -3,7 +3,7 @@ var swiper = new Swiper(".x-slider", {
   grabCursor: true,
   centeredSlides: true,
   //slidesPerView: "auto",
-  slidesPerView: 4,
+  slidesPerView: 3,
   spaceBetween: 0,
   loop: true,
   coverflowEffect: {
@@ -13,9 +13,10 @@ var swiper = new Swiper(".x-slider", {
     modifier: 2.0,
     slideShadows: true,
   },
-//  pagination: {
-//    el: ".swiper-pagination",
-//  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
   keyboard: true,
   autoplay: {
     delay: 3000,
@@ -36,6 +37,8 @@ swiper.on("keyPress", (swiper, keyCode) => {
   }
 });
 
+const ps4_select = new Audio("ps4-select-button.mp3");
+
 swiper.on("slideChange", function () {
   //console.log(this.realIndex);
   const index_currentSlide = this.realIndex;
@@ -45,39 +48,38 @@ swiper.on("slideChange", function () {
   this.slides.forEach((element, idx) => {
     element.classList.remove("contrast");
     element.classList.add("blur");
+    ps4_select.play();
     //element.classList.remove("scale-up-center");
-    //element.classList.add("scale-down-center");	
+    //element.classList.add("scale-down-center");
   });
   console.log(index_activeSlide);
   currentSlide.classList.remove("blur");
   currentSlide.classList.add("contrast");
   //currentSlide.classList.remove("scale-down-center");
-  //currentSlide.classList.add("scale-up-center");  
+  //currentSlide.classList.add("scale-up-center");
 });
 
-function clickButton(){
-	swiper.autoplay.stop();
-	Swal.fire({
-	  imageUrl: "https://placeholder.pics/svg/300x300",
-	  imageHeight: 100,
-	  imageAlt: "A tall image",
-	  buttonsStyling: true,
-	  showCancelButton: true,
-	  confirmButtonText: 'Confirm',
-	  cancelButtonText: 'Back',
-	  customClass: {
-		confirmButton: 'my-confirm-button-class',
-		cancelButton: 'my-cancel-button-class'
-	  }	  
-	}).then((result) => {
-	  if (result.isConfirmed) {
-
-		swiper.autoplay.start();
-		// Perform your delete action here
-	  } else if (result.dismiss === Swal.DismissReason.cancel) {
-
-		// Perform your cancel action here
-		swiper.autoplay.start();
-	  }
-	});
+function clickButton() {
+  // swiper.autoplay.stop();
+  // Swal.fire({
+  //   imageUrl: "https://placeholder.pics/svg/300x300",
+  //   imageHeight: 100,
+  //   imageAlt: "A tall image",
+  //   buttonsStyling: true,
+  //   showCancelButton: true,
+  //   confirmButtonText: "Confirm",
+  //   cancelButtonText: "Back",
+  //   customClass: {
+  //     confirmButton: "my-confirm-button-class",
+  //     cancelButton: "my-cancel-button-class",
+  //   },
+  // }).then((result) => {
+  //   if (result.isConfirmed) {
+  //     swiper.autoplay.start();
+  //     // Perform your delete action here
+  //   } else if (result.dismiss === Swal.DismissReason.cancel) {
+  //     // Perform your cancel action here
+  //     swiper.autoplay.start();
+  //   }
+  // });
 }
