1cc9d69b6349f183229ea42f916a41c19a21b06d
rjjrbatarao <50077340+rjjrbatarao@users.noreply.github.com>
2026-07-22 23:24:53 +0800

    Add files via upload

--- loader/script.js
+++ loader/script.js
@@ -1,15 +1,35 @@
 function triggerToast() {
             // Check if running inside our Android WebView container
-            if (window.TaraBridge) {
+        if (window.TaraBridge) {
                 window.TaraBridge.showToast("Hello from Webview JS!");
             } else {
                 console.log("Not running inside Android WebView container");
             }
-        }
+}
 
-        function fetchVersion() {
-            if (window.TaraBridge) {
-                const version = window.TaraBridge.getAppVersion();
-                console.log("Native App Version: " + version);
-            }
+function fetchVersion() {
+        if (window.TaraBridge) {
+            const version = window.TaraBridge.getAppVersion();
+            console.log("Native App Version: " + version);
         }
+}
+		
+function getDeviceInfo() {
+    if (window.TaraBridge) {
+        const info = {
+            osVersion: window.TaraBridge.getOsVersion(),
+            sdkVersion: window.TaraBridge.getSdkInt(),
+            deviceModel: window.TaraBridge.getDeviceModel(),
+            manufacturer: window.TaraBridge.getManufacturer(),
+            appVersion: window.TaraBridge.getAppVersion(),
+            batteryLevel: window.TaraBridge.getBatteryLevel() + "%",
+            isCharging: window.TaraBridge.isCharging()
+        };
+
+        console.log("Device System Info:", info);
+        return info;
+    } else {
+        console.warn("TaraBridge interface not found");
+        return null;
+    }
+}
