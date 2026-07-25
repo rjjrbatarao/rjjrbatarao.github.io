9ec94bc2289464b1558f25f9cec8c9a473eb7f66
rjjrbatarao <50077340+rjjrbatarao@users.noreply.github.com>
2026-07-26 01:26:23 +0800

    Add files via upload

--- menu/sw.js
+++ menu/sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'app-assets-v1';
 // Assets to cache immediately
-const PRECACHE_ASSETS = ['/', '/index.html', '/style.css', '/script.js', '/obrajs.js', 'sst.ttf', '/templates/app_item.html','/templates/app_layout.html','/templates/header_layout.html'];
+const PRECACHE_ASSETS = ['/menu', '/menu/index.html', '/menu/style.css', '/menu/script.js', '/menu/obrajs.js', '/menu/sst.ttf', '/menu/templates/app_item.html','/menu/templates/app_layout.html','/menu/templates/header_layout.html'];
 
 // Install: Cache core files
 self.addEventListener('install', event => {
