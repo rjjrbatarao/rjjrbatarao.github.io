78281991d69803ca0e35432dfe6643bc158a3b0c
rjjrbatarao <50077340+rjjrbatarao@users.noreply.github.com>
2026-08-20 12:20:53 +0800

    Add files via upload

--- smenu2/sw.js
+++ smenu2/sw.js
@@ -1,20 +1,28 @@
 const CACHE_NAME = 'app-assets-v1';
 // Assets to cache immediately
 const ASSETS_TO_CACHE = [
-    '/sloader2',
-    '/sloader2/index.html',
-    '/sloader2/style.css',
-    '/sloader2/index.js',
-    '/sloader2/obrajs.js',
-    '/sloader2/midori.umd.js',
-    '/sloader2/three.min.js',
-    '/sloader2/settings.js',
-    '/sloader2/sst.ttf',
-    '/sloader2/templates/coin_modal.html',
-    '/sloader2/templates/user_timer.html',
-    '/sloader2/templates/settings.html',
-    '/sloader2/beep.mp3',
-];
+    '/smenu2',
+    '/smenu2/index.html',
+    '/smenu2/style.css',
+    '/smenu2/index.js',
+    '/smenu2/obrajs.js',
+    '/smenu2/midori.umd.js',
+    '/smenu2/three.min.js',
+    '/smenu2/sst.ttf',
+    '/smenu2/templates/app_icon.html',
+    '/smenu2/templates/app_info.html',
+    '/smenu2/templates/app_item.html',
+    '/smenu2/templates/app_layout.html',
+    '/smenu2/templates/app_recents_item.html',
+    '/smenu2/templates/app_recents_layout.html',
+    '/smenu2/templates/header_layout.html',
+    '/smenu2/templates/marqueue_layout.html',
+    '/smenu2/templates/modal_game_mode.html',
+    '/smenu2/templates/coin_modal.html',
+    '/smenu2/templates/user_time.html',
+    '/smenu2/ps4-select-button.mp3',
+    '/smenu2/slide.mp3',
+    '/smenu2/beep.mp3'];
 
 // 1. Install & Cache
 self.addEventListener('install', (event) => {
