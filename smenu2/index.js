6dfa7930a5179e1da65a4c445038befd3a2b0ae2
rjjrbatarao <50077340+rjjrbatarao@users.noreply.github.com>
2026-09-18 21:07:55 +0800

    Add files via upload

--- smenu2/index.js
+++ smenu2/index.js
@@ -444,9 +444,14 @@ const renderUserTime = () => {
       if (coinTimer != null) {
         clearTimeout(coinTimer);
       }
-      coinFunc();
-      tara.oId('coinModal').show();
-      window.TaraBridge.sendBleCommand("DATA:ON");
+      const bluetoothState = window.TaraBridge.isBluetoothConnected();
+      if (bluetoothState) {
+        coinFunc();
+        tara.oId('coinModal').show();
+        window.TaraBridge.sendBleCommand("DATA:ON");
+      } else {
+        window.TaraBridge.showToast("Credit Terminal not connected!");
+      }
     }
   });
 }
